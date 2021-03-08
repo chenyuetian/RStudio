e14f3c326b8135d26d88d57485d06b4a01acd0ef
chenyuetian <chenyuetian1984@gmail.com>
2021-03-07 21:58:23 -0600

    added if.R

---
+++ if.R
@@ -0,0 +1,10 @@
+x <- 10
+if (x > 3 && x < 5) {
+  print("x is between 3 and 5")
+} else if (x <= 3){
+  print("x is less or equal to 3")
+} else {
+  browser()
+  print("x is greater or equal to 5")
+}
+
